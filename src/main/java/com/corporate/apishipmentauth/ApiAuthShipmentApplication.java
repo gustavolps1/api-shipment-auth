caffaeb32e33917cfff2d1f115c40a8eafe9b709
Gustavo Lopes <gustavolopes.dev@gmail.com>
2022-08-07 22:21:52 -0300

    first commit

---
+++ src/main/java/com/corporate/apishipmentauth/ApiAuthShipmentApplication.java
@@ -0,0 +1,13 @@
+package com.corporate.apishipmentauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAuthShipmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAuthShipmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/corporate/apishipmentauth/ApiAuthShipmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corporate.apishipmentauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAuthShipmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
